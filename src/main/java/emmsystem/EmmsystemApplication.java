d74fe6769a7f2e058abbc1bacf3f8dfe21a88255
sysdest <115469909+sishengduo@users.noreply.github.com>
2023-09-22 11:39:35 +0800

    Add files via upload

---
+++ src/main/java/emmsystem/EmmsystemApplication.java
@@ -0,0 +1,13 @@
+package emmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmmsystemApplication.class, args);
+    }
+
+}
